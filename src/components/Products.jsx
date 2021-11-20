4f05ca0a5130d6dfe2d5ad19d90a6da58d09022f
Wahab Khan <kwahab789@gmail.com>
2021-11-20 15:43:18 +0500

    working

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,6 +6,10 @@ const Products = () => {
       <h1 className="text-lg font-bold my-8 mx-14">Products</h1>
       <div className="grid grid-cols-5 my-8 gap-24 mx-20">
         <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
       </div>
     </div>
   );
