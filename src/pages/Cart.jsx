5dde4f14127a9343532856e3a4427914d4d2883c
Wahab Khan <kwahab789@gmail.com>
2021-11-24 18:00:14 +0500

    add selected items into cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,26 +1,57 @@
-import { React, useContext, useEffect } from "react";
+import { React, useContext, useEffect, useState } from "react";
 import { CartContext } from "../CartContex";
+// import Product from "../components/Product";
 
 const Cart = () => {
+  const [products, setProducts] = useState([]);
   const { cart } = useContext(CartContext);
   useEffect(() => {
     if (!cart.items) {
       return;
     }
-    console.log(Object.keys(cart.items));
-    // fetch("Product/cart-items", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": application / json,
-    //   },
-    //   body: { ids: Object.keys(cart.items) },
-    // });
-  }, []);
+    fetch("Product/cart-items", {
+      method: "POST",
+      headers: {
+        "Content-Type": " application / json",
+      },
+      body: JSON.stringify({ ids: Object.keys(cart.items) }),
+    })
+      .then((res) => res.json())
+      .then((products) => {
+        setProducts(products);
+      });
+  }, [cart]);
   return (
     <div className="container mx-auto lg:w-1/2 w-full pb-24">
       <h1 className="my-12 font-bold">Cart Items</h1>
       <ul>
-        <li className="mb-12">
+        {products.map((product) => {
+          return (
+            <li className="mb-12">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <img className="h-16" src="/images/peproni.png" alt="pizza" />
+                  <span className="font-bold ml-4 w-48">Chicken Specail</span>
+                </div>
+                <div>
+                  <button className="bg-yellow-500 px-4 py-2 rounded-full leading-none">
+                    -
+                  </button>
+                  <b className="mx-4">5</b>
+                  <button className="bg-yellow-500 px-4 py-2 rounded-full leading-none">
+                    +
+                  </button>
+                </div>
+                <span>Rs. 400</span>
+                <button className="bg-red-500 px-4 py-2 rounded-full leading-none text-white">
+                  Delete
+                </button>
+              </div>
+            </li>
+          );
+        })}
+
+        {/* <li className="mb-12">
           <div className="flex items-center justify-between">
             <div className="flex items-center">
               <img className="h-16" src="/images/peproni.png" alt="pizza" />
@@ -40,28 +71,7 @@ const Cart = () => {
               Delete
             </button>
           </div>
-        </li>
-        <li className="mb-12">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <img className="h-16" src="/images/peproni.png" alt="pizza" />
-              <span className="font-bold ml-4 w-48">Chicken Specail</span>
-            </div>
-            <div>
-              <button className="bg-yellow-500 px-4 py-2 rounded-full leading-none">
-                -
-              </button>
-              <b className="mx-4">5</b>
-              <button className="bg-yellow-500 px-4 py-2 rounded-full leading-none">
-                +
-              </button>
-            </div>
-            <span>Rs. 400</span>
-            <button className="bg-red-500 px-4 py-2 rounded-full leading-none text-white">
-              Delete
-            </button>
-          </div>
-        </li>
+        </li> */}
       </ul>
       <hr />
       <div className="text-right">
